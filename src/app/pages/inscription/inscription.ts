add40ca0491aecfb5cd6334b518cb7955e2ebe0d
dramekh <khoyane.drame@etu.univ-grenoble-alpes.fr>
2026-07-25 23:45:58 +0200

    page inscription KOFIS City

--- src/app/pages/inscription/inscription.ts
+++ src/app/pages/inscription/inscription.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Navbar } from '../../shared/navbar/navbar';
 
 @Component({
   selector: 'app-inscription',
-  imports: [],
+  imports: [FormsModule, Navbar],
   templateUrl: './inscription.html',
-  styleUrl: './inscription.css',
+  styleUrl: './inscription.css'
 })
-export class Inscription {}
+export class Inscription {
+  prenom = '';
+  nom = '';
+  email = '';
+  ville = '';
+  pole = '';
+
+  constructor(private router: Router) {}
+
+  inscrire() {
+    if (this.prenom && this.nom) {
+      localStorage.setItem('kofis_user', JSON.stringify({
+        prenom: this.prenom,
+        nom: this.nom,
+        ville: this.ville,
+        pole: this.pole
+      }));
+      this.router.navigate(['/dashboard']);
+    }
+  }
+}
