266eb562ccb11d20c36232e425a7c24e46da7d72
dramekh <khoyane.drame@etu.univ-grenoble-alpes.fr>
2026-07-26 11:12:15 +0200

    dashboard KOFIS City

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Navbar } from '../../shared/navbar/navbar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Navbar, RouterLink],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+  prenom = '';
+  nom = '';
+  ville = '';
+  pole = '';
+  initiales = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const data = localStorage.getItem('kofis_user');
+    if (data) {
+      const user = JSON.parse(data);
+      this.prenom = user.prenom;
+      this.nom = user.nom;
+      this.ville = user.ville;
+      this.pole = user.pole;
+      this.initiales = user.prenom[0] + user.nom[0];
+    } else {
+      this.router.navigate(['/inscription']);
+    }
+  }
+}
