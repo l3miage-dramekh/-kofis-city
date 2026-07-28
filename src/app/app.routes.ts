dacbdddc724c6e86fc4bb626789b0224b48bd536
dramekh <khoyane.drame@etu.univ-grenoble-alpes.fr>
2026-07-28 02:59:33 +0200

    garde de route - dashboard protégé

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { Inscription } from './pages/inscription/inscription';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { PoleEducation } from './pages/pole-education/pole-education';
 import { Connexion } from './pages/connexion/connexion';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: Accueil },
   { path: 'inscription', component: Inscription },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'education', component: PoleEducation },
   { path: 'connexion', component: Connexion },
+  { path: 'education', component: PoleEducation },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
