b2f05b8cc2b9e63cc4fe256412893215e23adfa6
dramekh <khoyane.drame@etu.univ-grenoble-alpes.fr>
2026-07-28 02:30:30 +0200

    déconnexion avec confirmation + navbar dynamique

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {}
+export class Navbar {
+  constructor(private router: Router) {}
+
+  estConnecte(): boolean {
+    return !!localStorage.getItem('kofis_token');
+  }
+
+deconnecter() {
+  const confirmation = window.confirm('Tu veux vraiment te déconnecter de KOFIS City ?');
+  if (confirmation) {
+    localStorage.removeItem('kofis_token');
+    localStorage.removeItem('kofis_user');
+    this.router.navigate(['/']);
+  }
+}
+}
